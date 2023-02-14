fce641dfeca91b61032a3ece7a74af83517f1bcf
Detjon Mataj <detjon.mataj@gmail.com>
2023-02-14 04:31:36 +0100

    Issue #18: Implemented dropdown type on NavbarItem
    - ListItem for building html block based on the given model.
    - UListModel for specifying the model of the Unordered List. It extends LinkModel functionality by adding a field for storing the list items.

---
+++ models/UListModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class UListModel extends LinkModel
+{
+    private array $items;
+    public function __construct($label, $url, $icon, $classes, $items)
+    {
+        parent::__construct($label, $url, $icon, $classes);
+        $this->items = $items;
+    }
+
+    public function attributes(): array
+    {
+        return parent::attributes()[] = ['items' => $this->items ];
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
