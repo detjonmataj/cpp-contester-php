7d0dd1f24f1ca3fe3e5709c7cdfc91e7ae5dc4ef
Detjon Mataj <detjon.mataj@gmail.com>
2023-02-14 05:06:57 +0100

    Issue #17: Added initial items on the administrator navbar
    - APP name [href="/"]
    - Home [href="/"]
    - Questions (Drop Down)
    - All questions [href="/questions"]
    - Categories [href="/question_categories"]
    - Levels [href="/question_levels"]
    - Name and Surname [href="/account"]
    - Settings (gear icon) [href="/settings"]
    
    When clicking the application name (Navbar Header), the application will redirect to the root page.

--- views/components/navbar.admin.php
+++ views/components/navbar.admin.php
@@ -1,10 +1,18 @@
 <?php
 require_once 'core/Navbar.php';
+require_once 'core/ListItem.php';
 
 $navbar = new Navbar(
-    (new NavbarItem(new LinkModel(Application::$APP->name(), '#', '', '')))->header(),
+    (new NavbarItem(new LinkModel(Application::$APP->name(), '/', '', '')))->header(),
     [
         (new NavbarItem(new LinkModel('Home', '/', '', '')))->link(),
+        (new NavbarItem(new UListModel('Questions', '#', '', '', [
+            (new ListItem(new LinkModel('All questions', 'questions', '', 'dropdown-item'))),
+            (new ListItem(new LinkModel('Categories', 'question_categories', '', 'dropdown-item'))),
+            (new ListItem(new LinkModel('Levels', 'question_levels', '', 'dropdown-item'))),
+        ])))->dropdown(),
+        (new NavbarItem(new LinkModel(Application::$APP->getUser()?->getDisplayName(), '/logout', '', '')))->link(),
+        (new NavbarItem(new LinkModel('⚙', '/settings', '', '')))->link(),
         (new NavbarItem(new LinkModel('Logout', '/logout', '', '')))->link(),
     ]
 );
