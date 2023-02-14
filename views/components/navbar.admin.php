5af2e85555512cc94add80a2d40b062ead2b41be
Detjon Mataj <detjon.mataj@gmail.com>
2023-02-14 06:57:45 +0100

    #Frontend: Fix the endpoint the field of username on navbar #22

--- views/components/navbar.admin.php
+++ views/components/navbar.admin.php
@@ -7,11 +7,11 @@ $navbar = new Navbar(
     [
         (new NavbarItem(new LinkModel('Home', '/', '', '')))->link(),
         (new NavbarItem(new UListModel('Questions', '#', '', '', [
-            (new ListItem(new LinkModel('All questions', 'questions', '', 'dropdown-item'))),
+            (new ListItem(new LinkModel('All Questions', 'questions', '', 'dropdown-item'))),
             (new ListItem(new LinkModel('Categories', 'question_categories', '', 'dropdown-item'))),
             (new ListItem(new LinkModel('Levels', 'question_levels', '', 'dropdown-item'))),
         ])))->dropdown(),
-        (new NavbarItem(new LinkModel(Application::$APP->getUser()?->getDisplayName(), '/logout', '', '')))->link(),
+        (new NavbarItem(new LinkModel(Application::$APP->getUser()?->getDisplayName(), '/account', '', '')))->link(),
         (new NavbarItem(new LinkModel('⚙', '/settings', '', '')))->link(),
         (new NavbarItem(new LinkModel('Logout', '/logout', '', '')))->link(),
     ]
